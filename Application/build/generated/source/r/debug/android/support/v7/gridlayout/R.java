3812c446b6c94b7b5cbf1f9eb6cb7c4c58be46c5
Ajbobtom <josh.r.perkins@gmail.com>
2017-04-22 23:39:46 -0600

    Fully functional and tested version. Release candidate #1 Added debug text for later user tested with gps spoofer

--- Application/build/generated/source/r/debug/android/support/v7/gridlayout/R.java
+++ Application/build/generated/source/r/debug/android/support/v7/gridlayout/R.java
@@ -8,47 +8,47 @@ package android.support.v7.gridlayout;
 
 public final class R {
 	public static final class attr {
-		public static final int alignmentMode = 0x7f0100e3;
-		public static final int columnCount = 0x7f0100e1;
-		public static final int columnOrderPreserved = 0x7f0100e5;
-		public static final int layout_column = 0x7f0100e9;
-		public static final int layout_columnSpan = 0x7f0100ea;
-		public static final int layout_columnWeight = 0x7f0100eb;
-		public static final int layout_gravity = 0x7f0100ec;
-		public static final int layout_row = 0x7f0100e6;
-		public static final int layout_rowSpan = 0x7f0100e7;
-		public static final int layout_rowWeight = 0x7f0100e8;
-		public static final int orientation = 0x7f0100df;
-		public static final int rowCount = 0x7f0100e0;
-		public static final int rowOrderPreserved = 0x7f0100e4;
-		public static final int useDefaultMargins = 0x7f0100e2;
+		public static final int alignmentMode = 0x7f01012f;
+		public static final int columnCount = 0x7f01012d;
+		public static final int columnOrderPreserved = 0x7f010131;
+		public static final int layout_column = 0x7f010135;
+		public static final int layout_columnSpan = 0x7f010136;
+		public static final int layout_columnWeight = 0x7f010137;
+		public static final int layout_gravity = 0x7f010138;
+		public static final int layout_row = 0x7f010132;
+		public static final int layout_rowSpan = 0x7f010133;
+		public static final int layout_rowWeight = 0x7f010134;
+		public static final int orientation = 0x7f01012b;
+		public static final int rowCount = 0x7f01012c;
+		public static final int rowOrderPreserved = 0x7f010130;
+		public static final int useDefaultMargins = 0x7f01012e;
 	}
 	public static final class dimen {
-		public static final int default_gap = 0x7f070051;
+		public static final int default_gap = 0x7f090075;
 	}
 	public static final class id {
-		public static final int alignBounds = 0x7f0b0024;
-		public static final int alignMargins = 0x7f0b0025;
-		public static final int bottom = 0x7f0b0026;
-		public static final int center = 0x7f0b0027;
-		public static final int center_horizontal = 0x7f0b0028;
-		public static final int center_vertical = 0x7f0b0029;
-		public static final int clip_horizontal = 0x7f0b002a;
-		public static final int clip_vertical = 0x7f0b002b;
-		public static final int end = 0x7f0b002c;
-		public static final int fill = 0x7f0b002d;
-		public static final int fill_horizontal = 0x7f0b002e;
-		public static final int fill_vertical = 0x7f0b002f;
-		public static final int horizontal = 0x7f0b0022;
-		public static final int left = 0x7f0b0030;
-		public static final int right = 0x7f0b0031;
-		public static final int start = 0x7f0b0032;
-		public static final int top = 0x7f0b0033;
-		public static final int vertical = 0x7f0b0023;
+		public static final int alignBounds = 0x7f0e003f;
+		public static final int alignMargins = 0x7f0e0040;
+		public static final int bottom = 0x7f0e0041;
+		public static final int center = 0x7f0e0042;
+		public static final int center_horizontal = 0x7f0e0043;
+		public static final int center_vertical = 0x7f0e0044;
+		public static final int clip_horizontal = 0x7f0e0045;
+		public static final int clip_vertical = 0x7f0e0046;
+		public static final int end = 0x7f0e0047;
+		public static final int fill = 0x7f0e0048;
+		public static final int fill_horizontal = 0x7f0e0049;
+		public static final int fill_vertical = 0x7f0e004a;
+		public static final int horizontal = 0x7f0e003d;
+		public static final int left = 0x7f0e004b;
+		public static final int right = 0x7f0e004c;
+		public static final int start = 0x7f0e004d;
+		public static final int top = 0x7f0e004e;
+		public static final int vertical = 0x7f0e003e;
 	}
 	public static final class styleable {
-		public static final int[] GridLayout = { 0x7f0100df, 0x7f0100e0, 0x7f0100e1, 0x7f0100e2, 0x7f0100e3, 0x7f0100e4, 0x7f0100e5 };
-		public static final int[] GridLayout_Layout = { 0x010100f4, 0x010100f5, 0x010100f6, 0x010100f7, 0x010100f8, 0x010100f9, 0x010100fa, 0x7f0100e6, 0x7f0100e7, 0x7f0100e8, 0x7f0100e9, 0x7f0100ea, 0x7f0100eb, 0x7f0100ec };
+		public static final int[] GridLayout = { 0x7f01012b, 0x7f01012c, 0x7f01012d, 0x7f01012e, 0x7f01012f, 0x7f010130, 0x7f010131 };
+		public static final int[] GridLayout_Layout = { 0x010100f4, 0x010100f5, 0x010100f6, 0x010100f7, 0x010100f8, 0x010100f9, 0x010100fa, 0x7f010132, 0x7f010133, 0x7f010134, 0x7f010135, 0x7f010136, 0x7f010137, 0x7f010138 };
 		public static final int GridLayout_Layout_android_layout_height = 1;
 		public static final int GridLayout_Layout_android_layout_margin = 2;
 		public static final int GridLayout_Layout_android_layout_marginBottom = 6;
--- Application/src/main/java/com/example/android/MotoHelm/MainActivity.java
+++ Application/src/main/java/com/example/android/MotoHelm/MainActivity.java
@@ -17,6 +17,7 @@
 
 package com.example.android.MotoHelm;
 
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -24,6 +25,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.ViewAnimator;
 
 import com.example.android.common.activities.SampleActivityBase;
@@ -34,6 +36,36 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 /**
  * A simple launcher activity containing a summary sample description, sample log and a custom
@@ -41,12 +73,43 @@ import com.google.android.gms.common.api.GoogleApiClient;
  * <p>
  * For devices with displays with a width of 720dp or greater, the sample log is always visible,
  * on other devices it's visibility is controlled by an item on the Action Bar.
- */
-public class MainActivity extends  SampleActivityBase {
+ **/
+public class MainActivity extends  SampleActivityBase implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     public static final String TAG = "MainActivity";
     BluetoothChatFragment BTFragment;
     Switch SW_Speed;
+
+    //Things that do requests
+    static final Integer LOCATION = 0x1;
+    static final Integer CALL = 0x2;
+    static final Integer WRITE_EXST = 0x3;
+    static final Integer READ_EXST = 0x4;
+    static final Integer CAMERA = 0x5;
+    static final Integer ACCOUNTS = 0x6;
+    static final Integer GPS_SETTINGS = 0x7;
+    //Request codes are any number I want...kinda stupid i guess.
+    static final Integer REQUEST_ENABLE_BT = 0x8;
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mLastLocation;
+
+    protected String mLatitudeLabel;
+    protected String mLongitudeLabel;
+    protected TextView mLatitudeText;
+    protected TextView mLongitudeText;
+    GoogleApiClient client1;
+    LocationRequest mLocationRequest;
+    PendingResult<LocationSettingsResult> result;
+
+    //enbales the things
 
     // Whether the Log Fragment is currently shown
     private boolean mLogShown;
@@ -67,6 +130,19 @@ public class MainActivity extends  SampleActivityBase {
             transaction.replace(R.id.sample_content_fragment, BTFragment);
             transaction.commit();
         }
+        SharedValues.FirstTime = true; //Reset to true every time we create the program.
+        SharedValues.ArrayFilled = false;
+        SharedValues.DirecRequest = false; // false tell we hit the submit button then we can begin the rest of the program.
+        /**********/
+        client1 = new GoogleApiClient.Builder(this) //Creating a google Api Client in the wrongest way possible
+                .addConnectionCallbacks(this)//Connection Call backs
+                .addOnConnectionFailedListener(this)//WE FAILED AT LISTENNING
+                .addApi(AppIndex.API) //APP index api
+                .addApi(LocationServices.API) //Locaiton services api
+                .build(); ///Build dis wall mr. trump
+        TryPermissions(); //Lets try to see if we have any permission...I mean...we don't like our locations
+        //to be no consentual right?
+        startService(new Intent(this, LocationSErvice.class));
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
@@ -150,6 +226,7 @@ public class MainActivity extends  SampleActivityBase {
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client.connect();
+        client1.connect();
         AppIndex.AppIndexApi.start(client, getIndexApiAction());
     }
 
@@ -161,7 +238,136 @@ public class MainActivity extends  SampleActivityBase {
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
         client.disconnect();
+        client1.disconnect();
     }
+
+//inserting new code below from Permissions app
+private void askForPermission(String permission, Integer requestCode) {
+    if (ContextCompat.checkSelfPermission(MainActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
+
+        // Should we show an explanation?
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permission)) {
+
+            //This is called if user has denied the permission before
+            //In this case I am just asking the permission again
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+
+        } else {
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+        }
+    } else {
+        //Granted Permisions why bother
+    }
+}
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+            switch (requestCode) {
+                //Location
+                case 1:
+                    //try {
+                    // askForGPS();
+                    // } catch (InterruptedException e) {
+                    //   e.printStackTrace();
+                    //   }
+                    break;
+                //Call
+                case 2:
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:" + "{This is a telephone number}"));
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        startActivity(callIntent);
+                    }
+                    break;
+                //Write external Storage
+                case 3:
+                    break;
+            }
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void askForGPS() throws InterruptedException {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(30 * 1000);
+        mLocationRequest.setFastestInterval(5 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        builder.setAlwaysShow(true);
+        result = LocationServices.SettingsApi.checkLocationSettings(client, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            status.startResolutionForResult(MainActivity.this, GPS_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        break;
+                }
+            }
+        });
+
+    }
+    /**** Permissions **/
+    public void TryPermissions() {
+        askForPermission(Manifest.permission.ACCESS_FINE_LOCATION, LOCATION);
+
+    }
+
+    public void onConnected(@Nullable Bundle bundle) {
+// Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            TryPermissions();
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(client1);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        android.util.Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        android.util.Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
+
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        client.disconnect();
+        client1.disconnect();
+        stopService(new Intent(this, LocationSErvice.class));
+    }
+
 
 
 
